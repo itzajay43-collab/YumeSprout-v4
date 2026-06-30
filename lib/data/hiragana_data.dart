4ddf6e08e9b035f02479f16cd9ce8330e4b21d88
Ajay Yadav <itzajay43@gmail.com>
2026-06-30 13:11:58 +0530

    Added complete 46 Hiragana data

--- lib/data/hiragana_data.dart
+++ lib/data/hiragana_data.dart
@@ -35,4 +35,267 @@ const List<HiraganaModel> hiragana = [
     meaning: "Tea",
     example: "おちゃ",
   ),
+// K Row
+const HiraganaModel(
+  jp: "か",
+  romaji: "Ka",
+  meaning: "Mosquito",
+  example: "か",
+),
+const HiraganaModel(
+  jp: "き",
+  romaji: "Ki",
+  meaning: "Tree",
+  example: "き",
+),
+const HiraganaModel(
+  jp: "く",
+  romaji: "Ku",
+  meaning: "Sky",
+  example: "くも",
+),
+const HiraganaModel(
+  jp: "け",
+  romaji: "Ke",
+  meaning: "Hair",
+  example: "け",
+),
+const HiraganaModel(
+  jp: "こ",
+  romaji: "Ko",
+  meaning: "Child",
+  example: "こども",
+),
+
+// S Row
+const HiraganaModel(
+  jp: "さ",
+  romaji: "Sa",
+  meaning: "Fish",
+  example: "さかな",
+),
+const HiraganaModel(
+  jp: "し",
+  romaji: "Shi",
+  meaning: "Poem",
+  example: "し",
+),
+const HiraganaModel(
+  jp: "す",
+  romaji: "Su",
+  meaning: "Watermelon",
+  example: "すいか",
+),
+const HiraganaModel(
+  jp: "せ",
+  romaji: "Se",
+  meaning: "Back",
+  example: "せ",
+),
+const HiraganaModel(
+  jp: "そ",
+  romaji: "So",
+  meaning: "Sky",
+  example: "そら",
+),
+
+// T Row
+const HiraganaModel(
+  jp: "た",
+  romaji: "Ta",
+  meaning: "Field",
+  example: "たんぼ",
+),
+const HiraganaModel(
+  jp: "ち",
+  romaji: "Chi",
+  meaning: "Father",
+  example: "ちち",
+),
+const HiraganaModel(
+  jp: "つ",
+  romaji: "Tsu",
+  meaning: "Moon",
+  example: "つき",
+),
+const HiraganaModel(
+  jp: "て",
+  romaji: "Te",
+  meaning: "Hand",
+  example: "て",
+),
+const HiraganaModel(
+  jp: "と",
+  romaji: "To",
+  meaning: "Door",
+  example: "とびら",
+),
+// N Row
+const HiraganaModel(
+  jp: "な",
+  romaji: "Na",
+  meaning: "Name",
+  example: "なまえ",
+),
+const HiraganaModel(
+  jp: "に",
+  romaji: "Ni",
+  meaning: "Garden",
+  example: "にわ",
+),
+const HiraganaModel(
+  jp: "ぬ",
+  romaji: "Nu",
+  meaning: "Cloth",
+  example: "ぬの",
+),
+const HiraganaModel(
+  jp: "ね",
+  romaji: "Ne",
+  meaning: "Cat",
+  example: "ねこ",
+),
+const HiraganaModel(
+  jp: "の",
+  romaji: "No",
+  meaning: "Drink",
+  example: "のむ",
+),
+
+// H Row
+const HiraganaModel(
+  jp: "は",
+  romaji: "Ha",
+  meaning: "Flower",
+  example: "はな",
+),
+const HiraganaModel(
+  jp: "ひ",
+  romaji: "Hi",
+  meaning: "Fire",
+  example: "ひ",
+),
+const HiraganaModel(
+  jp: "ふ",
+  romaji: "Fu",
+  meaning: "Ship",
+  example: "ふね",
+),
+const HiraganaModel(
+  jp: "へ",
+  romaji: "He",
+  meaning: "Room",
+  example: "へや",
+),
+const HiraganaModel(
+  jp: "ほ",
+  romaji: "Ho",
+  meaning: "Book",
+  example: "ほん",
+),
+
+// M Row
+const HiraganaModel(
+  jp: "ま",
+  romaji: "Ma",
+  meaning: "Window",
+  example: "まど",
+),
+const HiraganaModel(
+  jp: "み",
+  romaji: "Mi",
+  meaning: "Water",
+  example: "みず",
+),
+const HiraganaModel(
+  jp: "む",
+  romaji: "Mu",
+  meaning: "Purple",
+  example: "むらさき",
+),
+const HiraganaModel(
+  jp: "め",
+  romaji: "Me",
+  meaning: "Eye",
+  example: "め",
+),
+const HiraganaModel(
+  jp: "も",
+  romaji: "Mo",
+  meaning: "Peach",
+  example: "もも",
+),
+
+// Y Row
+const HiraganaModel(
+  jp: "や",
+  romaji: "Ya",
+  meaning: "Mountain",
+  example: "やま",
+),
+const HiraganaModel(
+  jp: "ゆ",
+  romaji: "Yu",
+  meaning: "Snow",
+  example: "ゆき",
+),
+const HiraganaModel(
+  jp: "よ",
+  romaji: "Yo",
+  meaning: "Night",
+  example: "よる",
+),
+
+// R Row
+const HiraganaModel(
+  jp: "ら",
+  romaji: "Ra",
+  meaning: "Camel",
+  example: "らくだ",
+),
+const HiraganaModel(
+  jp: "り",
+  romaji: "Ri",
+  meaning: "Apple",
+  example: "りんご",
+),
+const HiraganaModel(
+  jp: "る",
+  romaji: "Ru",
+  meaning: "Ruby",
+  example: "ルビー",
+),
+const HiraganaModel(
+  jp: "れ",
+  romaji: "Re",
+  meaning: "Zero",
+  example: "れい",
+),
+const HiraganaModel(
+  jp: "ろ",
+  romaji: "Ro",
+  meaning: "Corridor",
+  example: "ろうか",
+),
+
+// W Row
+const HiraganaModel(
+  jp: "わ",
+  romaji: "Wa",
+  meaning: "I",
+  example: "わたし",
+),
+const HiraganaModel(
+  jp: "を",
+  romaji: "Wo",
+  meaning: "Object Marker",
+  example: "を",
+),
+const HiraganaModel(
+  jp: "ん",
+  romaji: "N",
+  meaning: "Sound N",
+  example: "ほん",
+),
+
 ];
